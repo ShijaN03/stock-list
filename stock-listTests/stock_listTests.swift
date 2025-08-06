88eaae22523e4b58beafbfc27e325e8f7ec7f73f
ShijaN03 <shijan.ks@yandex.ru>
2025-08-06 14:54:19 +0300

    initial commit

---
+++ stock-listTests/stock_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  stock_listTests.swift
+//  stock-listTests
+//
+//  Created by shijan on 06.08.2025.
+//
+
+import Testing
+@testable import stock_list
+
+struct stock_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
